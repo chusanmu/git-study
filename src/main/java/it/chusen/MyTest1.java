e978fba6abd2d35b069ca6ad23a8465472dd70bc
chusen <chusen@ruigushop.com>
2020-11-10 22:59:51 +0800

    test future

--- src/main/java/it/chusen/MyTest1.java
+++ src/main/java/it/chusen/MyTest1.java
@@ -11,6 +11,8 @@ public class MyTest1 {
     public static void main(String[] args) {
         System.out.println("hello world master.");
         System.out.println("add one line");
+
+        System.out.println("test future ");
     }
 
 }
