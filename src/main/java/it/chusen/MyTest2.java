0f308b62498d1f44e90fc3dd4ef1e242868e208c
chusen <chusen@ruigushop.com>
2020-11-12 20:02:10 +0800

    本地的第二次提交

--- src/main/java/it/chusen/MyTest2.java
+++ src/main/java/it/chusen/MyTest2.java
@@ -9,6 +9,7 @@ public class MyTest2 {
     public static void main(String[] args) {
         System.out.println("i love coder and money");
         System.out.println("hello java");
-        System.out.println("jjjiji");
+          System.out.println("jjjiji");
+System.out.println();
     }
 }
