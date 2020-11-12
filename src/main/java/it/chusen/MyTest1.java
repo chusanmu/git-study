4c798601379a4430e38be3b59967b9a95e473420
chusen <chusen@ruigushop.com>
2020-11-12 20:02:38 +0800

    本地的第三次提交

--- src/main/java/it/chusen/MyTest1.java
+++ src/main/java/it/chusen/MyTest1.java
@@ -9,7 +9,7 @@ public class MyTest1 {
 
 
     public static void main(String[] args) {
-        System.out.println("hello world master.");
+        System.out.println("hello nihao world master.");
         System.out.println("xixi");
         for (int i = 0; i < 10; i++) {
             System.out.println("xx");
