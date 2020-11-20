b02dd984e1d2940f05513201c1a5822e113a7ea1
chusen <chusen12@163.com>
2020-11-20 15:22:11 +0800

    fix:update

--- src/main/java/it/chusen/MyTest1.java
+++ src/main/java/it/chusen/MyTest1.java
@@ -24,6 +24,11 @@ public class MyTest1 {
 
         System.out.println("test future ");
         System.out.println("update");
+
+        System.out.println("哈哈哈哈哈 你好");
+
+
+        System.out.println("你好呀弟弟");
     }
 
 }
